3da5770eb99cfb609c6975709270dee37fd0d66c
brainard23 <aidenyale00@gmail.com>
2023-05-29 18:11:23 +0800

    update

--- app/listings/[listingId]/page.tsx
+++ app/listings/[listingId]/page.tsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import getCurrentUser from "@/app/actions/getCurrentUsers";
+import getListingById from "@/app/actions/getListingById"
+import ClientOnly from "@/app/components/ClientOnly";
+import EmptyState from "@/app/components/EmptyState";
+import ListingClient from "./ListingClient";
 
-const ListingPage = () => {
+interface IParams {
+  listingId?: string,
+}
+
+const ListingPage = async ({ params }: { params: IParams }) => {
+  const listing = await getListingById(params);
+  const currentUser = await getCurrentUser();
+
+  if (!listing) {
+    return (
+      <ClientOnly>
+        <EmptyState />
+      </ClientOnly>
+    )
+  }
   return (
-    <div>page</div>
+    <ClientOnly>
+      <ListingClient
+        listing={listing}
+        currentUser={currentUser}
+      />
+    </ClientOnly>
   )
 }
 
